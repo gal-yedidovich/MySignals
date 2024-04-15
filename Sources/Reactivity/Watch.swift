4a9ae2134cc8a859cd8469d4f7a999d32bff5634
Gal Yedidovich <gal.yedidovich@unity3d.com>
2024-04-15 22:34:08 +0300

    REPLACED enum implementation with protocol

--- Sources/Reactivity/Watch.swift
+++ Sources/Reactivity/Watch.swift
@@ -7,33 +7,10 @@
 
 import Foundation
 
-private enum ReactiveValue<Value: Equatable> {
-	case signal(Signal<Value>)
-	case computed(Computed<Value>)
-	
-	var value: Value {
-		switch self {
-		case .signal(let signal):
-			return signal.value
-		case .computed(let computed):
-			return computed.value
-		}
-	}
-	
-	var source: Source {
-		switch self {
-		case .signal(let signal):
-			return signal.source
-		case .computed(let computed):
-			return computed.source
-		}
-	}
-}
-
 public class Watch<WatchedValue: Equatable> {
 	private let handler: (WatchedValue, WatchedValue) -> Void
 	private var currentValue: WatchedValue
-	private let reactiveValue: ReactiveValue<WatchedValue>
+	private let reactiveValue: any ReactiveValue<WatchedValue>
 	
 	private lazy var observer = {
 		Observer { [weak self] in
@@ -44,7 +21,7 @@ public class Watch<WatchedValue: Equatable> {
 	public init(_ signal: Signal<WatchedValue>, handler: @escaping (WatchedValue, WatchedValue) -> Void) {
 		self.handler = handler
 		self.currentValue = signal.value
-		self.reactiveValue = .signal(signal)
+		self.reactiveValue = signal
 		
 		signal.source.add(observer: observer)
 	}
@@ -52,7 +29,7 @@ public class Watch<WatchedValue: Equatable> {
 	public init(_ computed: Computed<WatchedValue>, handler: @escaping (WatchedValue, WatchedValue) -> Void) {
 		self.handler = handler
 		self.currentValue = computed.value
-		self.reactiveValue = .computed(computed)
+		self.reactiveValue = computed
 		
 		computed.source.add(observer: observer)
 	}
@@ -67,3 +44,13 @@ public class Watch<WatchedValue: Equatable> {
 		reactiveValue.source.remove(observer: observer)
 	}
 }
+
+
+protocol ReactiveValue<Value> {
+	associatedtype Value: Equatable
+	var value: Value { get }
+	var source: Source { get }
+}
+
+extension Signal: ReactiveValue {}
+extension Computed: ReactiveValue {}
