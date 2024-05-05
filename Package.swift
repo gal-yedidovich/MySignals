a8beca877df0a6529fa102150230c3a90a0a1160
Gal Yedidovich <gal.yedidovich@gmail.com>
2024-05-05 21:39:06 +0300

    MINOR reformatting

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Reactivity",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Reactivity",
-            targets: ["Reactivity"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Reactivity"),
-        .testTarget(
-            name: "ReactivityTests",
-            dependencies: ["Reactivity"]),
-    ]
+	name: "Reactivity",
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "Reactivity",
+			targets: ["Reactivity"]),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "Reactivity"),
+		.testTarget(
+			name: "ReactivityTests",
+			dependencies: ["Reactivity"]),
+	]
 )
